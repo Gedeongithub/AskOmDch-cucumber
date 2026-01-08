c5352ff8f065e8eca8bbea2844aedaebdbf1272e
Gedeon DUFITIMANA <gedeon.dufitimana2023@kepler.org>
2026-01-08 11:45:13 +0200

    Add productsList feature and its step files

---
+++ src/test/java/stepdefinitions/ProductsListingSteps.java
@@ -0,0 +1,29 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.*;
+
+public class ProductsListingSteps {
+    @Given("I am on the landing page of AskOmDch ecommerce")
+    public void i_am_on_the_landing_page_of_ask_om_dch_ecommerce() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I click the Store button")
+    public void i_click_the_store_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("I should be taken to the Store product listing page")
+    public void i_should_be_taken_to_the_store_product_listing_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("I should see a list of available products")
+    public void i_should_see_a_list_of_available_products() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
